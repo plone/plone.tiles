bb1a87a6ae84f4cd610f2120fb2bd2977516e4d3
Maurits van Rees <maurits@vanrees.org>
2020-03-26 22:49:47 +0100

    Added Plone Core to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
         'Framework :: Plone :: 5.2',
+        'Framework :: Plone :: Core',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
