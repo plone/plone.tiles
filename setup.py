dcc6dfbbd7b7359ed5c2f482c2d6f559a9b02f52
Maurits van Rees <maurits@vanrees.org>
2020-04-17 13:30:21 +0200

    Do not extend qa.cfg.  Test on 4.3 and 5.1 again.
    If the code-analysis and coverage parts could only be installed and executed on one job, that would be helpful.
    They are always the parts that cause the most breakage when running a buildout for an older Plone.

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.1',
         'Framework :: Plone :: 5.2',
         'Framework :: Plone :: Core',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
